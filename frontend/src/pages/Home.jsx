10defcdbbf5c7b2462074b5d1861af8321c92069
Saul Muñoz <sam.pedreros14@gmail.com>
2024-06-25 22:05:46 -0400

    Se agrega Dashboard a Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Dashboard } from "@/components/Dashboard";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Dashboard />
     </>
   );
 };
