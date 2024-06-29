d7ae0470100c16bbda5e6df755caa2f3c1aa0997
Saul Muñoz <sam.pedreros14@gmail.com>
2024-06-29 01:27:26 -0400

    Se actualiza navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { logout } from "@/services/auth.service.js";
 import { useToast } from "@/components/ui/use-toast";
 import {
@@ -14,11 +14,12 @@ import profileImage from "@/assets/png/avatar.png";
 
 import {
   Car,
-  CircleHelp,
   LayoutDashboard,
+  PanelsTopLeft,
   ParkingSquare,
   Settings,
   User,
+  Users,
 } from "lucide-react";
 import {
   TooltipProvider,
@@ -30,11 +31,8 @@ import { Link } from "react-router-dom";
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 
 const Navbar = () => {
-  const location = useLocation();
   const navigate = useNavigate();
 
-  const storedUser = JSON.parse(sessionStorage.getItem("usuario"));
-  const userRole = storedUser?.data?.rolName;
   const { toast } = useToast();
 
   const logoutSubmit = () => {
@@ -82,7 +80,7 @@ const Navbar = () => {
                   className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                   prefetch={false}
                 >
-                  <UsersIcon className="h-5 w-5" />
+                  <Users className="h-5 w-5" />
                   <span className="sr-only">Usuarios</span>
                 </Link>
               </TooltipTrigger>
@@ -157,7 +155,7 @@ const Navbar = () => {
           <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline" className="sm:hidden">
-                <PanelLeftIcon className="h-5 w-5" />
+                <PanelsTopLeft className="h-5 w-5" />
                 <span className="sr-only">Toggle Menu</span>
               </Button>
             </SheetTrigger>
@@ -184,7 +182,7 @@ const Navbar = () => {
                   className="flex items-center gap-4 px-2.5 text-foreground"
                   prefetch={false}
                 >
-                  <UsersIcon className="h-5 w-5" />
+                  <Users className="h-5 w-5" />
                   Usuarios
                 </Link>
                 <Link
@@ -237,66 +235,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-function Package2Icon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z" />
-      <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.8 1.1L21 9" />
-      <path d="M12 3v6" />
-    </svg>
-  );
-}
-
-function PanelLeftIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="18" height="18" x="3" y="3" rx="2" />
-      <path d="M9 3v18" />
-    </svg>
-  );
-}
-
-function UsersIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-      <circle cx="9" cy="7" r="4" />
-      <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-      <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-    </svg>
-  );
-}
