2d55f8c91fdb0aa8ecfb0d2420eb1956ccbfb1cc
Saul Muñoz <sam.pedreros14@gmail.com>
2024-06-29 02:54:18 -0400

    Se actualizan MainLayout

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -62,8 +62,8 @@ const Navbar = () => {
               className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
               prefetch={false}
             >
-              <ParkingSquare className="h-4 w-4 transition-all group-hover:scale-110" />
-              <span className="sr-only">Acme Inc</span>
+              <ParkingSquare className="h-4 w-4 transition-all group-hover:scale-150" />
+              <span className="sr-only">Epark</span>
             </Link>
             <Tooltip>
               <TooltipTrigger asChild>
