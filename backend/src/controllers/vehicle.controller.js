daf3e951f0170e31152f995ec1e17492eb6c0057
matias <xgalladex1234@gmail.com>
2024-07-02 21:03:59 -0400

    agregado busqueda de vehicle por id o por dueño  con params

--- backend/src/controllers/vehicle.controller.js
+++ backend/src/controllers/vehicle.controller.js
@@ -30,6 +30,7 @@ export async function createVehicle(req, res)  {
         res.status(500).json({ message: error.message });
     }
 };
+
 export async function getVehicles(req, res) {
     try {
         const vehicles = await Vehicle.find().populate("user", "username");
@@ -42,4 +43,61 @@ export async function getVehicles(req, res) {
         res.status(500).json({ message: error.message });
     }
 }
-  
+
+export async function getVehicleById(req,res) {
+    try {
+        const id =req.params.id;
+        const vehicle = await Vehicle.findById(id).populate("user", "username");
+
+        if(!vehicle){
+            res.status(400).json({
+                message: "vehiculo no encontrado",
+                data: null,
+            })
+        }
+
+        res.status(200).json({
+            message: "vehicle encontrado",
+            data: vehicle,
+        })
+    } catch (error) {
+        console.log("Error en getVehicleById(): ", error);
+        return null;
+    }
+}
+
+
+export async function getVehicleByOwnerId(req, res) {
+    try {
+        const userId = req.params.id;
+        
+        // Verificar que el usuario exista
+        const user = await User.findById(userId);
+        if (!user) {
+            res.status(404).json({
+                message: "Usuario no encontrado",
+                data: null,
+            });
+            return;
+        }
+
+        // Buscar el vehículo asociado al usuario
+        const vehicle = await Vehicle.findOne({ user: userId }).populate("user", "username");
+        if (!vehicle) {
+            res.status(404).json({
+                message: "El usuario no posee vehículo",
+                data: null,
+            });
+            return;
+        }
+
+        // Retornar el vehículo encontrado
+        res.status(200).json({
+            message: "Vehículo encontrado",
+            data: vehicle,
+        });
+    } catch (error) {
+        console.log("Error en getVehicleByOwnerId(): ", error);
+        res.status(500).json({ message: error.message });
+    }
+}
