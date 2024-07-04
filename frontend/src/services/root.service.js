e16484f2625c6f2170daa466570b1d243cbfd58e
bawssy <ibawssy@gmail.com>
2024-07-04 17:28:24 -0400

    Creacion rama Tomas

--- frontend/src/services/root.service.js
+++ frontend/src/services/root.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_BASE_URL || "http://localhost:7777/api";
 
 const instance = axios.create({
   baseURL: API_URL,
