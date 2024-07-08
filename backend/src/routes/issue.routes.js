7440b1cf5b1d9e64111d0ed551d35d163bc1542e
Saul Muñoz <sam.pedreros14@gmail.com>
2024-07-08 18:48:13 -0400

    Se agrega middleware a la ruta de Issues

--- backend/src/routes/issue.routes.js
+++ backend/src/routes/issue.routes.js
@@ -8,19 +8,22 @@ import {
   deleteIssue,
 } from "../controllers/issue.controller.js";
 
+// Se importa Middleware de autenticación
+import { isAdmin, isAdminOrUser } from "../middlewares/auth.middleware.js";
+
 // Se realiza una instancia de express
 const router = express.Router();
 
 // Ruta para crear una incidencia
-router.post("/createIssue/:id", createIssue);
+router.post("/createIssue/:id", isAdminOrUser, createIssue);
 
 // Ruta para obtener los reclamos de un usuario específico
-router.get("/getIssues/", getIssues);
+router.get("/getIssues/", isAdmin, getIssues);
 
 // Ruta para obtener todas las incidencias
-router.get("/getIssueById/:id", getIssueById);
+router.get("/getIssueById/:id", isAdmin, getIssueById);
 
 // Ruta para eliminar un reclamo específico
-router.delete("/deleteIssueById/:id", deleteIssue);
+router.delete("/deleteIssueById/:id", isAdmin, deleteIssue);
 
 export default router;
