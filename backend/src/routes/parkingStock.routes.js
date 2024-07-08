230af305a0ce7e96fa9041e4af906cd59227fe2f
Saul Muñoz <sam.pedreros14@gmail.com>
2024-07-08 18:44:35 -0400

    Se agregan middlewares a las rutas de los modelos de la base de datos

--- backend/src/routes/parkingStock.routes.js
+++ backend/src/routes/parkingStock.routes.js
@@ -2,35 +2,68 @@
 
 import { Router } from "express";
 
-import {initializeSpaces,createSpecifiedSpaces,createSpaceOnQuadrant,occupiesSpaceOnQuadrant,vacateSpaceOnQuadrant,getAllSpaces,getAvailableSpaces,getAvailableSpacesOnQuadrant,getOccupiedSpacesOnQuadrant,getOccupiedSpaces,deleteAllSpaces,deleteSpaceOnQuadrant,deleteSpecifiedSpacesOnQuadrant} from "../controllers/parkingStock.controller.js"
+import {
+  initializeSpaces,
+  createSpecifiedSpaces,
+  createSpaceOnQuadrant,
+  occupiesSpaceOnQuadrant,
+  vacateSpaceOnQuadrant,
+  getAllSpaces,
+  getAvailableSpaces,
+  getAvailableSpacesOnQuadrant,
+  getOccupiedSpacesOnQuadrant,
+  getOccupiedSpaces,
+  deleteAllSpaces,
+  deleteSpaceOnQuadrant,
+  deleteSpecifiedSpacesOnQuadrant,
+} from "../controllers/parkingStock.controller.js";
+
+// Se importa Middleware de autenticación
+import { isAdmin, isAdminOrUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 //Inicializar espacios (100 por default)
-router.post("/initializeSpaces",initializeSpaces);
+router.post("/initializeSpaces/:id", isAdmin, initializeSpaces);
 //Create cantidad especifica de espacios
-router.post("/createSpecifiedSpaces/:id",createSpecifiedSpaces);
+router.post("/createSpecifiedSpaces/:id", isAdmin, createSpecifiedSpaces);
 //Crear un solo espacio
-router.post("/createSpaceOnQuadrant/:id",createSpaceOnQuadrant)
+router.post("/createSpaceOnQuadrant/:id", isAdmin, createSpaceOnQuadrant);
 //Obtener todos los espacios
-router.get("/getAllSpaces",getAllSpaces);
+router.get("/getAllSpaces", isAdminOrUser, getAllSpaces);
 //Obtener espacios disponibles
-router.get("/getAvailableSpaces",getAvailableSpaces);
+router.get("/getAvailableSpaces", getAvailableSpaces);
 //Obtener espacios disponibles en un cuadrante
-router.get("/getAvailableSpacesOnQuadrant/:id",getAvailableSpacesOnQuadrant);
+router.get(
+  "/getAvailableSpacesOnQuadrant/:id",
+  isAdminOrUser,
+  getAvailableSpacesOnQuadrant,
+);
 //Obtener espacios ocupados en un cuadrante
-router.get("/getOccupiedSpacesOnQuadrant/:id",getOccupiedSpacesOnQuadrant);
+router.get(
+  "/getOccupiedSpacesOnQuadrant/:id",
+  isAdminOrUser,
+  getOccupiedSpacesOnQuadrant,
+);
 //Obtener espacios ocupados
-router.get("/getOccupiedSpaces",getOccupiedSpaces);
+router.get("/getOccupiedSpaces", isAdminOrUser, getOccupiedSpaces);
 //Ocupar un espacio
-router.put("/occupiesSpaceOnQuadrant/:id",occupiesSpaceOnQuadrant);
+router.put(
+  "/occupiesSpaceOnQuadrant/:id",
+  isAdminOrUser,
+  occupiesSpaceOnQuadrant,
+);
 //Desocupar un espacio
-router.put("/vacateSpaceOnQuadrant/:id",vacateSpaceOnQuadrant);
+router.put("/vacateSpaceOnQuadrant/:id", isAdminOrUser, vacateSpaceOnQuadrant);
 //Eliminar todos los espacios
-router.delete("/deleteAllSpaces",deleteAllSpaces);
+router.delete("/deleteAllSpaces", isAdmin, deleteAllSpaces);
 //Eliminar un espacio
-router.delete("/deleteSpaceOnQuadrant/:id",deleteSpaceOnQuadrant);
+router.delete("/deleteSpaceOnQuadrant/:id", isAdmin, deleteSpaceOnQuadrant);
 //Eliminar cantidad de espacios espesificados
-router.delete("/deleteSpecifiedSpacesOnQuadrant/:id",deleteSpecifiedSpacesOnQuadrant);
+router.delete(
+  "/deleteSpecifiedSpacesOnQuadrant/:id",
+  isAdmin,
+  deleteSpecifiedSpacesOnQuadrant,
+);
 
 export default router;
