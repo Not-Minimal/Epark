b7ada81e22c1328f77250ad019768106b0b99c12
matias <xgalladex1234@gmail.com>
2024-08-01 16:49:08 -0400

    edita color , edita alerta , agrega bredcrumb para poder devolverse

--- frontend/src/pages/protected/Vehicle/Dashboard.jsx
+++ frontend/src/pages/protected/Vehicle/Dashboard.jsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink } from "@/components/ui/breadcrumb";
 
 export default function Dashboard() {
   return (
     <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to="/vehicle/dashboard">Vehículo</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <main>
         <div className="flex flex-col items-center justify-center h-auto gap-4 ">
           <Link to="/vehicle/GetByLicensePlate">
@@ -11,7 +21,6 @@ export default function Dashboard() {
               Buscar vehículo por su patente
             </Button>
           </Link>
-
           <Link to="/vehicle/UpdateVehicleByLicensePlate">
             <Button className="w-64 bg-[#4CAF50] text-white hover:bg-[#45a049] px-8 py-3 rounded-md focus:outline-none">
               Editar vehículo según su patente
--- frontend/src/pages/protected/Vehicle/UpdateVehicleByLicensePlate.jsx
+++ frontend/src/pages/protected/Vehicle/UpdateVehicleByLicensePlate.jsx
@@ -1,26 +1,23 @@
 import React, { useState } from "react";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import { Link } from 'react-router-dom';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { getVehicleByLicensePlate, updatedVehicleByLicensePlate } from "@/services/vehicle.service.js";
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
-export default function GetByLicensePlate() {
+export default function UpdateVehicleByLicensePlate() {
   const [licensePlate, setLicensePlate] = useState("");
   const [vehicle, setVehicle] = useState(null);
   const [error, setError] = useState("");
+  const [alertVisible, setAlertVisible] = useState(false);
 
   const handleSearch = async () => {
     try {
-      const vehicleData = await getVehicleByLicensePlate(licensePlate);
-      setVehicle(vehicleData.data);
+      const response = await getVehicleByLicensePlate(licensePlate);
+      const vehicleData = response.data;
+      setVehicle(vehicleData);
       setError("");
     } catch (err) {
       setError(err.message || "Error al buscar el vehículo");
@@ -40,92 +37,111 @@ export default function GetByLicensePlate() {
     try {
       await updatedVehicleByLicensePlate(licensePlate, {
         licensePlate: vehicle.licensePlate,
-        model: vehicle.model,  // Asegúrate de que las claves coincidan con los nombres esperados en el controlador
+        model: vehicle.model,
         color: vehicle.color,
         brand: vehicle.brand,
       });
       setError("");
-      alert("Vehículo actualizado exitosamente");
+      setAlertVisible(true);
+      setTimeout(() => {
+        setAlertVisible(false);
+      }, 3000);
     } catch (err) {
       setError(err.message || "Error al actualizar el vehículo");
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-auto bg-stone-50">
-      <Card className="p-8 rounded-lg shadow-md w-full max-w-md">
-        <CardHeader>
-          <CardTitle className="text-2xl font-bold text-green-600">Buscar Patente</CardTitle>
-          <CardDescription className="text-gray-600">
-            Ingresa la patente de tu vehículo a editar
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <Input
-            type="text"
-            value={licensePlate}
-            onChange={(e) => setLicensePlate(e.target.value)}
-            placeholder="Ingresa la patente"
-            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent"
-          />
-          <Button
-            onClick={handleSearch}
-            className="w-full px-4 py-2 font-medium text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2"
-          >
-            Buscar
-          </Button>
-          {error && <p className="text-red-500 mt-4">{error}</p>}
-          
-          {vehicle && (
-            <div className="mt-4">
-              <CardContent>
-                <form className="grid gap-4">
-                  <div className="grid gap-2">
-                    <Label htmlFor="licensePlate">Patente</Label>
-                    <Input
-                      id="licensePlate"
-                      value={vehicle.licensePlate || ""}
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    <Label htmlFor="model">Modelo</Label>
-                    <Input
-                      id="model"
-                      value={vehicle.model || ""}
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    <Label htmlFor="color">Color</Label>
-                    <Input
-                      id="color"
-                      value={vehicle.color || ""}
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    <Label htmlFor="brand">Marca</Label>
-                    <Input
-                      id="brand"
-                      value={vehicle.brand || ""}
-                      onChange={handleChange}
-                    />
-                  </div>
-                </form>
-              </CardContent>
-              <CardFooter>
-                <Button
-                  onClick={handleEdit}
-                  className="w-full mt-4 font-medium text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2"
-                >
-                  Editar
-                </Button>
-              </CardFooter>
-            </div>
-          )}
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to="/vehicle/dashboard">Vehículo</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Editar Vehículo</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="flex flex-col items-center justify-center h-auto">
+        <Card className="p-8 rounded-lg shadow-md w-full max-w-md">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">Buscar Patente</CardTitle>
+            <CardDescription className="text-gray-600">Ingresa la patente de tu vehículo a editar</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <Input
+              type="text"
+              value={licensePlate}
+              onChange={(e) => setLicensePlate(e.target.value)}
+              placeholder="Ingresa la patente"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent"
+            />
+            <Button
+              onClick={handleSearch}
+              className="w-full px-4 py-2 font-medium text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2"
+            >
+              Buscar
+            </Button>
+            {error && <p className="text-red-500 mt-4">{error}</p>}
+            {vehicle && (
+              <div className="mt-4">
+                <CardContent>
+                  <form className="grid gap-4">
+                    <div className="grid gap-2">
+                      <Label htmlFor="licensePlate">Patente</Label>
+                      <Input
+                        id="licensePlate"
+                        value={vehicle.licensePlate || ""}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="model">Modelo</Label>
+                      <Input
+                        id="model"
+                        value={vehicle.model || ""}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="color">Color</Label>
+                      <Input
+                        id="color"
+                        value={vehicle.color || ""}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="brand">Marca</Label>
+                      <Input
+                        id="brand"
+                        value={vehicle.brand || ""}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <Button
+                      type="button"
+                      onClick={handleEdit}
+                      className="w-full px-4 py-2 font-medium text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2"
+                    >
+                      Actualizar Vehículo
+                    </Button>
+                  </form>
+                </CardContent>
+              </div>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+      {alertVisible && (
+        <div className="fixed bottom-4 right-4 bg-green-500 text-white py-2 px-4 rounded shadow-lg">
+          Vehículo actualizado exitosamente
+        </div>
+      )}
+    </>
   );
 }
