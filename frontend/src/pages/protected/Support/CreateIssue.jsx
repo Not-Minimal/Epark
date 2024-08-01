519d70c6ba15098899d65724d13e2ce038e9f4e5
hat-namco <nico2003426@gmail.com>
2024-07-31 22:24:31 -0400

    se agrega funcion de crear reclamos

--- frontend/src/pages/protected/Support/CreateIssue.jsx
+++ frontend/src/pages/protected/Support/CreateIssue.jsx
@@ -16,7 +16,36 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useNavigate } from "react-router-dom";
+import Form from "@/components/forms/Form";
+import { Link } from "react-router-dom";
+import { register } from "@/services/auth.service";
+import { createIssue } from "@/services/issue.service";
+
 export default function CreateIssue() {
+  const navigate = useNavigate();
+
+  const registerIssue = (data) => {
+    createIssue('6687461ad57cdae16403e03d',data)
+      .then(() => {
+        // Mostrar un toast de éxito
+        navigate("/support/dashboard"); // Redirige a la página de inicio de sesión
+      })
+      .catch((error) => {
+        // Mostrar un toast de error
+        console.error("Error registering:", error);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      title: event.target.title.value,
+      description: event.target.description.value,
+    };
+    registerIssue(data);
+  };
+
   return (
     <>
       <header>
@@ -46,15 +75,16 @@ export default function CreateIssue() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <form className="grid gap-4">
+              <form className="grid gap-4" onSubmit={handleSubmit}>
                 <div className="grid gap-2">
                   <Label htmlFor="title">Titulo</Label>
-                  <Input id="title" placeholder="Ingrese un titulo" />
+                  <Input id="title" name="title" placeholder="Ingrese un titulo" />
                 </div>
                 <div className="grid gap-2">
                   <Label htmlFor="description">Descripcion</Label>
                   <Textarea
                     id="description"
+                    name="description"
                     placeholder="Describe los detalles de tu reclamo"
                     rows={4}
                   />
